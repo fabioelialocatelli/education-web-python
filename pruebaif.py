bce63d4a1e17e27f7000a956fd3ebee364c70fa8
Fabio Elia Locatelli <fabioelialocatelli@yandex.com>
2018-06-05 15:43:15 +1200

    Control Statements
    Just started the course and going over control statements. Trivial so far.

---
+++ pruebaif.py
@@ -0,0 +1,15 @@
+miweb = "marisco"
+if miweb == "noticias":
+    print("web sobre noticias")
+elif miweb == "deportes":
+    print("web sobre deportes")
+elif miweb == "caballos":
+    print("web sobre caballos")
+else:
+    print("web de otra cosa")
+
+edad = 26
+mensaje = "Eres mayor de edad" if edad >= 18 else "Eres menor de edad"
+print(mensaje)
+
+print("programa finalizado")
